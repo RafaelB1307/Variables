9b61738209616eb433de591eeb3c089066634793
RafaelB1307 <Rafael.bastides@uabc.edu.mx>
2022-05-20 22:00:26 -0700

    Agregar archivos de proyecto.

---
+++ Calcular Area/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vala = 10;
+            int valb = 10;
+            int valc = 7;
+            float area = (vala*valb)/2;
+            Console.WriteLine("E1 AREA del triangulo es: +area");
+            Console.WriteLine("presiona cualquier tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
